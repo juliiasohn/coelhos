18bdde9de3e515de0362d4f918503678bba363b7
Julia <161714809+juliiasohn@users.noreply.github.com>
2024-10-15 07:58:17 -0300

    Add files via upload

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -2,7 +2,14 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
-document.querySelector(".info-peso");
+var tdPeso = paciente.querySelector(".info-peso");
 
+var peso = tdPeso.textContent;
 
-console.log(paciente);
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc")
+
+var imc = peso / (altura * altura); 
+tdImc.textContent = imc;
